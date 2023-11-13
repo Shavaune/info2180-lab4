52db2d80900da67af3ea68949ff552e452a146c8
Shavaune <114256220+Shavaune@users.noreply.github.com>
2023-11-12 23:11:07 -0500

    updated for 2023

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-window.addEventListener('load', ()=>{
+//Excercise 3
 
-    document.querySelector("button#btn").addEventListener("click", ()=>{
-        fetch("superheroes.php")
-        .then(resp => resp.text())
-        .then(data => alert(data) )
+window.onload = function(){
+    //Variables
+    var request = new XMLHttpRequest();
+    var btn = document.querySelector("#searchBtn");
+    var input = document.querySelector("#hero");
+    var output = document.querySelector("#result");
+    var url = "superheroes.php";
+    
+    btn.addEventListener('click', e =>{
+        e.preventDefault();
 
+        //AJAX request using XMLHttpRequest
+        request.onreadystatechange = e =>{
+            if(request.readyState === 4){
+                if(request.status === 200){
+                    var answer = request.responseText
+                    output.innerHTML = answer;
+                    elem.classList.add("not-found", answer);
+                    console.log(":D"); 
+                }
+                else{
+                    output.innerHTML = "An error occured X_X";
+                    console.log("D:");
+                }
+            }
+        }
+        console.log("test")
+        request.open("GET", url + "?query=" + input.value);
+        request.send();   
     });
-
-});
+}
